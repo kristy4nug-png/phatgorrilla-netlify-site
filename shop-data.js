cae047308e5d5091d1db2a0a851f9c1663b71c5c
Phat Gorrilla <kristy4nug@gmail.com>
2026-05-30 11:26:11 +0100

    Update Phat Gorrilla brand website and shop

---
+++ shop-data.js
@@ -0,0 +1,122 @@
+﻿window.PHAT_GORRILLA_PRODUCTS = [
+    {
+        "id":  "6a1a9c3b1babc69aaa0ed446",
+        "title":  "T‑Shirt — “Phat Gorrilla” Gold Logo Tee",
+        "price":  "From £22.40",
+        "image":  "https://images-api.printify.com/mockup/6a1a9c3b1babc69aaa0ed446/11962/102005/tshirt-phat-gorilla-gold-logo-tee.jpg?camera_label=front-2",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28973204",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a19dec1a0b34026ee0a16bb",
+        "title":  "Scotland Phat Gorrilla T-Shirt — Scottish Pride Rugby Fan Tee",
+        "price":  "From £32.12",
+        "image":  "https://images-api.printify.com/mockup/6a19dec1a0b34026ee0a16bb/12100/92570/scotland-phat-gorilla-t-shirt-scottish-pride-rugby-fan-tee.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28962356",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a19dd531babc69aaa0e422e",
+        "title":  "England Graphic Tee — \u0027Phat Gorrilla\u0027 London Skyline Football Fan Shirt",
+        "price":  "From £32.12",
+        "image":  "https://images-api.printify.com/mockup/6a19dd531babc69aaa0e422e/12022/102006/england-graphic-tee-phat-gorilla-london-skyline-football-fan-shirt.jpg?camera_label=back-2",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28962286",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a19dabc997906336605f3eb",
+        "title":  "Phat Gorrilla Logo Zip Hoodie — Luxury Gold Monogram Streetwear",
+        "price":  "£103.11",
+        "image":  "https://images-api.printify.com/mockup/6a19dabc997906336605f3eb/63249/5051/phat-gorrilla-logo-zip-hoodie-luxury-gold-monogram-streetwear.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28962025",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a19d8c9c03728859b04044a",
+        "title":  "Relaxed Hoodie — Minimal Gold \u0027B\u0027 Monogram Hoodie",
+        "price":  "£74.15",
+        "image":  "https://images-api.printify.com/mockup/6a19d8c9c03728859b04044a/115803/111733/relaxed-hoodie-minimal-gold-b-monogram-hoodie.jpg?camera_label=folded",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28961853",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a19d7bbbc484b92020c4a98",
+        "title":  "Minimalist Logo Tee — Gold \u0027PG\u0027 Monogram Chest T-Shirt",
+        "price":  "From £20.99",
+        "image":  "https://images-api.printify.com/mockup/6a19d7bbbc484b92020c4a98/12124/102005/minimalist-logo-tee-gold-pg-monogram-chest-t-shirt.jpg?camera_label=front-2",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28961774",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a18807721f4508fc9026c24",
+        "title":  "Wales Phat Gorrilla Graphic T-Shirt — Rugby/Football Fan Tee",
+        "price":  "From £36.42",
+        "image":  "https://images-api.printify.com/mockup/6a18807721f4508fc9026c24/73207/98446/wales-phat-gorilla-graphic-t-shirt-rugbyfootball-fan-tee.jpg?camera_label=back",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28940752",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a174e6e318117fcbc0505f7",
+        "title":  "Brazil Flag Graphic T‑Shirt — Rio Carnival \u0026 Football Fan Tee",
+        "price":  "From £32.12",
+        "image":  "https://images-api.printify.com/mockup/6a174e6e318117fcbc0505f7/12100/92570/brazil-flag-graphic-tshirt-rio-carnival-football-fan-tee.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28922404",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a174b81318117fcbc050461",
+        "title":  "Gorilla King Tee — Green Streetwear Graphic T-Shirt with \u0027PHAT\u0027 Design",
+        "price":  "From £23.86",
+        "image":  "https://images-api.printify.com/mockup/6a174b81318117fcbc050461/12100/92570/gorilla-king-tee-green-streetwear-graphic-t-shirt-with-phat-design.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28922238",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a1749a0a63666767e0ea50a",
+        "title":  "Phat Gorrilla Heavyweight Hoodie",
+        "price":  "From £64.52",
+        "image":  "https://images-api.printify.com/mockup/6a1749a0a63666767e0ea50a/123966/111658/unisex-heavyweight-hooded-sweatshirt.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28922345",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a1746c375f5c394ef0a8281",
+        "title":  "Phat Gorrilla Heavy Cotton Tee",
+        "price":  "From £23.86",
+        "image":  "https://images-api.printify.com/mockup/6a1746c375f5c394ef0a8281/12100/92570/unisex-heavy-cotton-tee.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28922351",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a17456d75f5c394ef0a8194",
+        "title":  "Gorilla Crewneck Sweatshirt — \u0027Phat Gorrilla\u0027 Retro Neon Logo",
+        "price":  "From £37.60",
+        "image":  "https://images-api.printify.com/mockup/6a17456d75f5c394ef0a8194/25458/98502/gorilla-crewneck-sweatshirt-phat-gorilla-retro-neon-logo.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28921663",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a173b4315c0ad1fc90f07c4",
+        "title":  "Phat Gorrilla Oversized Boxy Tee",
+        "price":  "From £36.76",
+        "image":  "https://images-api.printify.com/mockup/6a173b4315c0ad1fc90f07c4/103520/100285/unisex-oversized-boxy-tee.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28921541",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a17387f616126f69b0aec15",
+        "title":  "Phat Gorrilla Heavy Cotton Tee",
+        "price":  "From £33.21",
+        "image":  "https://images-api.printify.com/mockup/6a17387f616126f69b0aec15/12124/92570/unisex-heavy-cotton-tee.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28921544",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    },
+    {
+        "id":  "6a1730f7318117fcbc04f678",
+        "title":  "Paper Poster",
+        "price":  "From £17.38",
+        "image":  "https://images-api.printify.com/mockup/6a1730f7318117fcbc04f678/70655/11328/paper-poster.jpg?camera_label=front",
+        "buy_url":  "https://phat-gorrilla.printify.me/product/28920231",
+        "offer":  "Free Phat Gorrilla app access with first purchase"
+    }
+];
